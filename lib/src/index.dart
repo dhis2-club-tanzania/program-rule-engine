a06c2f5556832f7c636c311b39be2a6960636eda
josephatJ <josephatjulius24@gmail.com>
2022-01-10 10:42:56 +0300

    refactor: initial conversion to dart

---
+++ lib/src/index.dart
@@ -0,0 +1,9 @@
+export "services/engine.service.dart" show execute, executeWithAction;
+export "interfaces/rules-engine.types.dart"
+    show
+        ProgramRule,
+        EventData,
+        DataElements,
+        ProgramRuleVariable,
+        ProgramRuleAction,
+        OptionSets;
